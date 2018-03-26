ff0c90bd86e62446a1c2811bffbac1527f491237
Ashish Upreti <server@server.com>
2018-03-26 20:16:08 +0530

    First Commit

---
+++ app/src/main/java/com/ashishupreti/ashishupretixyz/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ashishupreti.ashishupretixyz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView mywebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView=(WebView)findViewById(R.id.webview);
+        WebSettings webSettings= mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.loadUrl("https://www.ashishupreti.xyz");
+        mywebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mywebView.canGoBack())
+        {
+            mywebView.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+
+}
